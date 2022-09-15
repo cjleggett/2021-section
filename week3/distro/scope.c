e970fb3e7a821946b1bbda17f704d106f7d8057a
Connor Leggett <43161915+cjleggett@users.noreply.github.com>
2022-09-15 16:06:33 -0400

    update for practice

--- week3/distro/scope.c
+++ week3/distro/scope.c
@@ -16,6 +16,7 @@ date;
 void change(int, date, int[]);
 void printarr(int[]);
 void print_date(date);
+void print_current_values(int, int, date, date, int[], int[]);
 
 // Declaring some variables
 date day1;
@@ -31,39 +32,18 @@ int main(void)
 
     // create a new date, integer, and array
     date day2;
-    day2.day = 26;
-    day2.year = 2020;
-    day2.month = "October";
+    day2.day = 25;
+    day2.year = 2019;
+    day2.month = "September";
 
-    int integer2 = 10;
-    int array2[ALENGTH] = {5, 4, 3, 2, 1};
+    int integer2 = 5;
+    int array2[ALENGTH] = {1, 2, 3, 4, 5};
 
-    // Prints current values:
-    printf("Integer 1: %i\n", integer1);
-    printf("Integer 2: %i\n", integer2);
-    printf("Day 1: ");
-    print_date(day1);
-    printf("Day 2: ");
-    print_date(day2);
-    printf("Array 1: ");
-    printarr(array1);
-    printf("Array 2: ");
-    printarr(array2);
+    print_current_values(integer1, integer2, day1, day2, array1, array2);
 
     change(integer2, day2, array2);
 
-    // Prints current values:
-    printf("\n\n");
-    printf("Integer 1: %i\n", integer1);
-    printf("Integer 2: %i\n", integer2);
-    printf("Day 1: ");
-    print_date(day1);
-    printf("Day 2: ");
-    print_date(day2);
-    printf("Array 1: ");
-    printarr(array1);
-    printf("Array 2: ");
-    printarr(array2);
+    print_current_values(integer1, integer2, day1, day2, array1, array2);
 
 }
 
@@ -73,12 +53,17 @@ void change(int integer2, date day2, int array2[])
     day1.month = "May";
     day2.month = "May";
 
-    integer1 += 1;
-    integer2 += 1;
+    integer1 += integer1;
+    integer2 += integer2;
 
     array1[0] = 20;
     array2[0] = 20;
 }
+
+
+
+// IGONORE BELOW HERE
+
 
 // Prints out a date struct
 void print_date(date d)
@@ -99,4 +84,19 @@ void printarr(int array[])
         }
     }
     printf("]\n");
+}
+
+void print_current_values(int i1, int i2, date d1, date d2, int a1[], int a2[])
+{
+    printf("\n\n");
+    printf("Integer 1: %i\n", i1);
+    printf("Integer 2: %i\n", i2);
+    printf("Day 1: ");
+    print_date(d1);
+    printf("Day 2: ");
+    print_date(d2);
+    printf("Array 1: ");
+    printarr(a1);
+    printf("Array 2: ");
+    printarr(a2);
 }
